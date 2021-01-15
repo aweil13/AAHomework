18ead5b7be7bf83abb0e6a190629e1ebdeb08985
alejandro <alejandroweil13@gmail.com>
2021-01-15 00:52:51 -0500

    Created and initialized Queue class with 3 methods: Queue#enqueue(pushes element into queue) Queue#dequeue(removes first element pushed into Queue) Queue#peek(shows first element in Queue)

---
+++ W3D5/queue.rb
@@ -0,0 +1,19 @@
+class Queue
+
+    def initialize
+        @queue = Array.new(0)
+    end
+
+    def enqueue(el)
+        @queue.push(el)
+    end
+
+    def dequeue
+        @queue.shift
+    end
+
+    def peek
+        @queue[0]
+    end
+
+end
